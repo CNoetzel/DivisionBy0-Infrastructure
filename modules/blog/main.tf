1bebedd1726e09b991407940fd4912948f243ce0
Carsten Noetzel <carsten.noetzel@gmail.com>
2023-10-31 12:54:19 +0100

    Remove certificate as it is not longer needed

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -41,40 +41,6 @@ resource "aws_route53_record" "nestingbox_record" {
   name    = "nestingbox.${local.domainName}"
   type    = "A"
   zone_id = aws_route53_zone.hosted_zone.id
-  records = ["77.1.154.28"]
+  records = ["77.8.156.16"]
   ttl     = 300
-}
-
-
-// ###################################################################################
-// Certificate
-// ###################################################################################
-resource "aws_acm_certificate" "domain_cert" {
-  domain_name               = "*.${local.domainName}"
-  subject_alternative_names = [local.domainName]
-  validation_method         = "DNS"
-  tags                      = local.tags
-}
-
-// Certificate validation records
-resource "aws_route53_record" "certificate_validation_records" {
-  for_each = {
-    for dvo in aws_acm_certificate.domain_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-  allow_overwrite = true
-  name            = each.value.name
-  type            = each.value.type
-  zone_id         = aws_route53_zone.hosted_zone.id
-  records         = [each.value.record]
-  ttl             = 60
-}
-
-// Certificate validation
-resource "aws_acm_certificate_validation" "domain_cert_validation" {
-  certificate_arn         = aws_acm_certificate.domain_cert.arn
-  validation_record_fqdns = []
 }
